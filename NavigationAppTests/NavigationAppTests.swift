6e45a2c921393a3c8ecb8f9e1e52dac70743a33e
Marc-Olivier Bergeron <mo_b@icloud.com>
2026-06-17 14:08:06 -0400

    Initial Commit

---
+++ NavigationAppTests/NavigationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationAppTests.swift
+//  NavigationAppTests
+//
+//  Created by Marc-Olivier Bergeron on 2026-06-17.
+//
+
+import Testing
+@testable import NavigationApp
+
+struct NavigationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
